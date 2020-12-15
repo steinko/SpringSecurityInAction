2d5608159c34da120879559b7e51721075e2b3c5
steinko <stein_korsveien@msn.com>
2020-12-15 16:28:28 +0100

    first commit

---
+++ src/test/java/com/laurentiuspilca/ssia/MainIT.java
@@ -0,0 +1,60 @@
+package com.laurentiuspilca.ssia;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.web.server.LocalServerPort;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.web.context.WebApplicationContext;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+public class MainIT {
+	
+	 @LocalServerPort
+	 private  int localServerPort;
+	 
+	 @Autowired
+	  private WebApplicationContext webApplicationContext;
+	
+	
+	 @Test
+     public void shouldbeUnautorised()  {
+    	 
+    	 
+    	 String url =  "http://localhost:" + localServerPort + "/hello";	
+    	 
+    	 given().
+          webAppContextSetup(webApplicationContext)
+        .when()
+           .get(url)
+        .then()
+          .statusCode(UNAUTHORIZED.value()); 
+     
+     }
+	 
+	 
+	 @Test
+     public void shouldAutorise()  {
+    	 
+    	 
+    	 String url =  "http://localhost:" + localServerPort + "/hello";	
+    	 
+    	 given()
+    	  .queryParam("user","steinko")
+          .webAppContextSetup(webApplicationContext)
+        .when()
+           .get(url)
+        .then()
+          .statusCode(OK.value()); 
+     
+     }
+
+}
